a0b0721c2cff69fd75cbb64c066b83b5a1034c1c
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-02-07 15:25:54 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
+
+	import Navbar from '../lib/navbar.svelte';
+
+	onMount(() => {
+  themeChange(false)
+})
+</script>
+
+<Navbar />
+
+<slot />
