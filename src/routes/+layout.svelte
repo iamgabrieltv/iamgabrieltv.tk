d2d76e442d9265654df5cb52c4c570fefb3cc04c
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-02-07 15:31:27 +0100

    Optimized Theme Changer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,7 @@
 <script>
 	import '../app.css';
 
-	import { onMount } from 'svelte';
-	import { themeChange } from 'theme-change';
-
 	import Navbar from '../lib/navbar.svelte';
-
-	onMount(() => {
-  themeChange(false)
-})
 </script>
 
 <Navbar />
