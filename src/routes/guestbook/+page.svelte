a64d2fd5ac325a44053ab79b34cb7c41ed493734
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-02-09 09:51:16 +0000

    Added login button

--- src/routes/guestbook/+page.svelte
+++ src/routes/guestbook/+page.svelte
@@ -1,27 +1,13 @@
 <script>
-	import { signIn, signOut } from '@auth/sveltekit/client';
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
-
-	onMount(function () {
-		console.log($page.data.session)
-	})
+	import LoginButton from '$lib/loginButton.svelte';
 </script>
 
-<h1>SvelteKit Auth Example</h1>
-<p>
-	{#if $page.data.session}
-		{#if $page.data.session.user?.image}
-			<span style="background-image: url('{$page.data.session.user.image}')" class="w-96" />
-		{/if}
-		<span class="w-full">
-			<small>Signed in as</small><br />
-			<strong>{$page.data.session.user?.name ?? 'User'}</strong>
-		</span>
-		<p>Session expiry: {$page.data.session?.expires}</p>
-		<button on:click={() => signOut()} class="button">Sign out</button>
-	{:else}
-		<span class="notSignedInText">You are not signed in</span>
-		<button on:click={() => signIn('github')}>Sign In with GitHub</button>
-	{/if}
-</p>
+<h1 class="text-3xl font-bold">Guestbook</h1>
+<p class="text-xl mb-2">Write something nice!</p>
+
+{#if $page.data.session}
+	<p class="text-lg">You are logged in as {$page.data.session.user?.name}</p>
+{:else}
+	<LoginButton />
+{/if}
