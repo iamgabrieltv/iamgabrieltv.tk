17bd2d90c7d9b4ab85905059c114a4fc431afc4d
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-02-10 20:27:02 +0100

    Fixed Bug: Could send messages that were NULL

--- src/routes/guestbook/+page.svelte
+++ src/routes/guestbook/+page.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 	import LoginButton from '$lib/loginButton.svelte';
 	import { supabase } from '$lib/supabase';
-	import {v4 as uuidv4} from 'uuid';
+	import { v4 as uuidv4 } from 'uuid';
 
 	let newMessage: string;
 
@@ -11,13 +11,20 @@
 	$: ({ messages } = data);
 
 	async function sendMessage() {
-		document.getElementById('input').value="";
-		const record = {username: $page.data.session?.user?.name, message: newMessage}
-		messages = [...messages, record];
+		if (newMessage) {
+			document.getElementById('input').value = '';
+			const record = { username: $page.data.session?.user?.name, message: newMessage };
+			messages = [...messages, record];
 
-		const { error } = await supabase
-  .from('guestbook')
-  .insert({ username: $page.data.session?.user?.name, message: newMessage, email: $page.data.session?.user?.email, id: uuidv4() })
+			const { error } = await supabase
+				.from('guestbook')
+				.insert({
+					username: $page.data.session?.user?.name,
+					message: newMessage,
+					email: $page.data.session?.user?.email,
+					id: uuidv4()
+				});
+		}
 	}
 </script>
 
@@ -25,10 +32,18 @@
 <p class="text-xl mb-2">Write something nice!</p>
 
 {#if $page.data.session}
-	<small>You are logged in as {$page.data.session.user?.name} ({$page.data.session.user?.email})</small>
+	<small
+		>You are logged in as {$page.data.session.user?.name} ({$page.data.session.user?.email})</small
+	>
 
 	<form on:submit|preventDefault={sendMessage}>
-		<input type="text" placeholder="Message" id="input" class="input w-full max-w-xs bg-base-200" bind:value={newMessage} />
+		<input
+			type="text"
+			placeholder="Message"
+			id="input"
+			class="input w-full max-w-xs bg-base-200"
+			bind:value={newMessage}
+		/>
 		<button type="submit" class="btn btn-ghost bg-base-300">Sign</button>
 	</form>
 {:else}
