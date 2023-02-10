7d781becfcb5e5647d0b38fa612ba3a37b620cb4
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-02-10 14:48:50 +0100

    Remove planetscale

--- src/lib/messages.svelte
+++ src/lib/messages.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
  import { page } from "$app/stores";
- import { queryBuilder } from "./planetscale";
 
  let email = $page.data.session?.user?.email;
  let username = $page.data.session?.user?.name;
@@ -9,19 +8,12 @@
 
  async function sendMessage() {
   if ($page.data.session) {
-   await queryBuilder
-    .insertInto('guestbook')
-    .values({
-     email,
-     body,
-     created_by: username,
-    })
-    .execute();
+   
  }
 }
 </script>
 
 <form on:submit|preventDefault={sendMessage}>
  <input placeholder="Message" type="text" class="input w-full max-w-xs bg-base-200 m-2" bind:value={body} />
- <button type="submit" class="btn">Sign</button>
+ <button type="submit" class="btn btn-ghost bg-base-300"><p>Sign</p></button>
 </form>
