478cd44dd77b55ddb12c5dc307fb6c8361f49229
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-03-01 21:59:53 +0100

    sync changes

--- src/routes/api/destiny/+server.ts
+++ src/routes/api/destiny/+server.ts
@@ -2,6 +2,8 @@ import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { BUNGIE_USER_ID, BUNGIE_USER_PLATFORM, BUNGIE_API_KEY } from '$env/static/private';
 
+let keyHeader = new Headers({ 'X-API-Key': BUNGIE_API_KEY });
+
 export const GET = (async () => {
 	const manifest = await (await fetch('https://www.bungie.net/Platform/Destiny2/Manifest/')).json();
 
@@ -9,5 +11,36 @@ export const GET = (async () => {
 		await fetch('https://bungie.net' + manifest.Response.jsonWorldContentPaths.en)
 	).json();
 
-	return json(contentManifest.DestinyActivityDefinition);
+	const profile = await (
+		await fetch(
+			'https://bungie.net/Platform/Destiny2/' +
+				BUNGIE_USER_PLATFORM +
+				'/Profile/' +
+				BUNGIE_USER_ID +
+				'?components=100,200,204',
+			{ headers: keyHeader }
+		)
+	).json();
+
+	function activityName() {
+		const characterActivities = profile.Response.characterActivities.data;
+		const keys = Object.keys(characterActivities);
+		const firstGuardian = characterActivities[keys[0]];
+		const currentActivityHash = firstGuardian.currentActivityHash;
+
+		switch (currentActivityHash) {
+			case 0:
+				return 'Offline';
+				break;
+			case 82913930:
+				return 'In Orbit';
+				break;
+			default:
+				const definitions = Object.keys(contentManifest.DestinyActivityDefinition);
+				const matchingObject = definitions.find((obj) => obj === currentActivityHash);
+				return definitions;
+		}
+	}
+
+	return json(activityName());
 }) satisfies RequestHandler;
---
+++ src/routes/debug/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const response = await (await fetch('/api/destiny')).json();
+		console.log(response);
+	});
+</script>
