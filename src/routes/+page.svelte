022580b21061aa6748564a3d0f985d6484bebddc
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-03-13 17:55:19 +0100

    Add transition and spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,21 +15,41 @@
 	You can also leave a message in the <a class="btn-link" href="/guestbook">Guestbook</a>!
 </p>
 
-<div class="flex flex-row mt-2">
+<div class="flex flex-row mt-2 gap-2">
 	<a href="https://github.com/iamgabrieltv">
-		<Icon class="hover:text-black" width="2em" height="2em" icon="mdi:github" />
+		<Icon
+			class="hover:text-black transition-all duration-[0.25s]"
+			width="2em"
+			height="2em"
+			icon="mdi:github"
+		/>
 	</a>
 
 	<a href="https://twitch.tv/iamgabrieltv">
-		<Icon class="hover:text-[#9147ff]" width="2em" height="2em" icon="mdi:twitch" />
+		<Icon
+			class="hover:text-[#9147ff] transition-all duration-[0.25s]"
+			width="2em"
+			height="2em"
+			icon="mdi:twitch"
+		/>
 	</a>
 
 	<a href="https://youtube.com/@iamgabrieltv">
-		<Icon class="hover:text-[#fe0000]" width="2em" height="2em" icon="mdi:youtube" />
+		<Icon
+			class="hover:text-[#fe0000] transition-all duration-[0.25s]"
+			width="2em"
+			height="2em"
+			icon="mdi:youtube"
+		/>
 	</a>
 
 	<a href="https://linktr.ee/iamgabrieltv">
-		<Icon class="hover:text-[#43e660]" width="2em" height="2em" icon="simple-icons:linktree" />
+		<Icon
+			class="hover:text-[#43e660] transition-all duration-[0.25s]"
+			width="2em"
+			height="2em"
+			icon="simple-icons:linktree"
+		/>
 	</a>
 </div>
 
