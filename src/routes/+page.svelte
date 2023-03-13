ec0d0a19cc35412339adf34b165b4ba12255060d
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-03-13 17:47:42 +0100

    Add social icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import DiscordRpc from '$lib/components/discord-rpc.svelte';
 
+	import Icon from '@iconify/svelte';
+
 	export let data;
 </script>
 
@@ -12,6 +14,24 @@
 <p class="text-lg">
 	You can also leave a message in the <a class="btn-link" href="/guestbook">Guestbook</a>!
 </p>
+
+<div class="flex flex-row mt-2">
+	<a href="https://github.com/iamgabrieltv">
+		<Icon class="hover:text-black" width="2em" height="2em" icon="mdi:github" />
+	</a>
+
+	<a href="https://twitch.tv/iamgabrieltv">
+		<Icon class="hover:text-[#9147ff]" width="2em" height="2em" icon="mdi:twitch" />
+	</a>
+
+	<a href="https://youtube.com/@iamgabrieltv">
+		<Icon class="hover:text-[#fe0000]" width="2em" height="2em" icon="mdi:youtube" />
+	</a>
+
+	<a href="https://linktr.ee/iamgabrieltv">
+		<Icon class="hover:text-[#43e660]" width="2em" height="2em" icon="simple-icons:linktree" />
+	</a>
+</div>
 
 <div class="divider" />
 
